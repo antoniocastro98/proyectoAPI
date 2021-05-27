ee87d81d60595e905b8f7c6971ad47e8534f3aee
root <antoniocastro-@hotmail.com>
2021-05-28 01:13:06 +0200

    añadida foto estatica de jugador

--- app.py
+++ app.py
@@ -7,32 +7,54 @@ key=os.getenv("key")
 cabeceras={"accept": "application/json","X-AUTH-TOKEN":key}
 app = Flask (__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET"])
 def inicio():
     return render_template("inicio.html")
 
 @app.route('/ligas', methods=["GET"])
 def ligas():
-    r=requests.get(URL_BASE+"api/leagues",headers=cabeceras)
-    if r.status_code == 200:
-        datos=r.json()
-        print(datos)
-
-@app.route('/jugadores')
-def jugadores():
-    cabeceras={"accept": "application/json","X-AUTH-TOKEN":key,"Content-Type": "application/json"}
-    datos="{\"name\":\"Cristiano Ronaldo\"}"
-    r=requests.post(URL_BASE+"api/players",headers=cabeceras,data=datos)
-
-    if r.status_code==200:
-        datos=r.json()
-        print(datos)
+    ligas=request.form.get(URL_BASE+'api/leagues', headers=headers)
+    if request.method=="GET":
+        return render_template("ligas.html")   
     else:
-        datos=r.json()
-        print(datos)
+        cabeceras={"accept": "application/json","X-AUTH-TOKEN":key"}
+        r=requests.post(URL_BASE+"api/players",headers=cabeceras)	
+        if r.status_code==200:
+            lista=[]
+            for i in datosjugador["items"]:
+            lista.append(i)
+        return render_template('ligas.html', ligas1=ligas1)
 
-#como funciona las repsuestas de las peticiones en la app
 
-port=os.environ["PORT"]
 
-app.run('0.0.0.0', int(port), debug=False)
+
+@app.route('/jugadores/',methods=["GET","POST"])
+def jugadores():
+    jugadores1=request.form.get("jugador")
+    if request.method=="GET":
+        return render_template("jugadores.html")    
+    else: 
+            cabeceras={"accept": "application/json","X-AUTH-TOKEN":key,"Content-Type": "application/json"}
+            datos="{\"name\":\"%s\"}" %jugadores1
+            r=requests.post(URL_BASE+"api/players",headers=cabeceras,data=datos)	
+            if r.status_code==200:
+                datosjugador=r.json()
+                lista=[]
+                for i in datosjugador["items"]:
+                    lista.append(i)
+                print(i)
+                id=datosjugador.get("items")[0].get("id")
+
+            return render_template("jugadores.html",datos=lista,id=id)	
+
+ 
+
+@app.route('/cartas')
+def cartas():
+        return render_template("cartas.html")
+
+
+
+
+
+app.run(debug=True)
