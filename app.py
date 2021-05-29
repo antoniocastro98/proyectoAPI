8772b550e353ffbe87c8b5fa731b9c21fa295739
root <antoniocastro-@hotmail.com>
2021-05-29 13:38:13 +0200

    añadido favicon

--- app.py
+++ app.py
@@ -13,17 +13,15 @@ def inicio():
 
 @app.route('/ligas', methods=["GET"])
 def ligas():
-    ligas=request.form.get(URL_BASE+'api/leagues', headers=headers)
-    if request.method=="GET":
-        return render_template("ligas.html")   
-    else:
-        cabeceras={"accept": "application/json","X-AUTH-TOKEN":key"}
-        r=requests.post(URL_BASE+"api/players",headers=cabeceras)	
+        cabeceras={"accept": "application/json","X-AUTH-TOKEN":key,"Content-Type": "application/json"}
+        r=requests.post(URL_BASE+"api/leagues",headers=cabeceras)	
         if r.status_code==200:
+            ligas1=r.json()
             lista=[]
-            for i in datosjugador["items"]:
-            lista.append(i)
-        return render_template('ligas.html', ligas1=ligas1)
+            for i in ligas1["items"]:
+                lista.append(i)
+        return render_template('ligas.html', ligas1=lista)
+
 
 
 
@@ -40,20 +38,35 @@ def jugadores():
             if r.status_code==200:
                 datosjugador=r.json()
                 lista=[]
+                ritmo=datosjugador.get("items")[0].get("pace")
+                pase=datosjugador.get("items")[0].get("passing")
+                disparo=datosjugador.get("items")[0].get("shooting")
+                fisico=datosjugador.get("items")[0].get("physicality") 
+                regate=datosjugador.get("items")[0].get("dribbling") 
+                defensa=datosjugador.get("items")[0].get("defending") 
                 for i in datosjugador["items"]:
                     lista.append(i)
-                print(i)
-                id=datosjugador.get("items")[0].get("id")
-
-            return render_template("jugadores.html",datos=lista,id=id)	
-
- 
-
-@app.route('/cartas')
-def cartas():
-        return render_template("cartas.html")
+                id1=datosjugador.get("items")[0].get("id")
+            return render_template("jugadores.html",datos=lista,id=id1,ritmo=ritmo,pase=pase,disparo=disparo,fisico=fisico,regate=regate,defensa=defensa)	
 
 
+@app.route('/precios', methods=["GET"])
+def precios():
+    jugadores1=request.form.get("jugador")    
+    if request.method=="GET":
+        return render_template("precios.html")   
+    else:
+        cabeceras={"accept": "application/json","X-AUTH-TOKEN":key,"Content-Type": "application/json"}
+        datos="{\"name\":\"%s\"}" %jugadores1
+        r=requests.post(URL_BASE+"api/players",headers=cabeceras,data=datos)
+        if r.status_code==200:
+            jugadores=r.json()
+            id2=jugadores.get("items")[0].get("id")
+        r2=requests.post(URL_BASE+"/api/players/%s/price",headers=cabeceras) %id2
+        if r2.status_code==200:
+            precios=r.json()
+
+        return render_template('precios.html', precios=lista2,id=id2)
 
 
 
