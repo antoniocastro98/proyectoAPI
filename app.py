fc2ebf1e47382c096eec649b140e833be227fc3a
root <antoniocastro-@hotmail.com>
2021-05-30 20:58:36 +0200

    edicion del fichero app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, abort, render_template, request
 import requests
 import json
 import os
+app = Flask (__name__)
 URL_BASE="https://futdb.app/"
 key=os.getenv("key")
 cabeceras={"accept": "application/json","X-AUTH-TOKEN":key}
-app = Flask (__name__)
+
 
 @app.route('/',methods=["GET"])
 def inicio():
